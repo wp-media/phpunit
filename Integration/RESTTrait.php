5b53bc6380f4848bfa20df335cb11788ea70acbc
Tonya Mork <tonya@wp-media.me>
2020-04-07 18:03:17 -0500

    Replaces rest server set up with `rest_get_server()`.
    Co-Authored-By: Grégory Viguier <Screenfeed@users.noreply.github.com>

--- Integration/RESTTrait.php
+++ Integration/RESTTrait.php
@@ -22,9 +22,7 @@ trait RESTTrait {
 		/**
 		 * @var WP_REST_Server $wp_rest_server
 		 */
-		global $wp_rest_server;
-		$this->server = $wp_rest_server = new WP_REST_Server;
-		do_action( 'rest_api_init' );
+		$this->server = rest_get_server();
 	}
 
 	/**
