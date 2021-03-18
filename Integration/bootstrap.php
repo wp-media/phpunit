3b5e860ed8cbafca32422e54936032afff4c2a5d
Rémy Perona <remperona@gmail.com>
2021-03-18 08:43:50 -0400

    update bootstrap with WPIntegration functions

--- Integration/bootstrap.php
+++ Integration/bootstrap.php
@@ -4,47 +4,22 @@ namespace WPMedia\PHPUnit\Integration;
 
 use WPMedia\PHPUnit\BootstrapManager;
 use function WPMedia\PHPUnit\init_test_suite;
+use Yoast\WPTestUtils\WPIntegration;
 
+require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
 require_once dirname( dirname( __FILE__ ) ) . '/bootstrap-functions.php';
-init_test_suite( 'Integration' );
-
-/**
- * Get the WordPress' tests suite directory.
- *
- * @return string Returns The directory path to the WordPress testing environment.
- */
-function get_wp_tests_dir() {
-	$tests_dir = getenv( 'WP_TESTS_DIR' );
-
-	// Travis CI & Vagrant SSH tests directory.
-	if ( empty( $tests_dir ) ) {
-		$tests_dir = '/tmp/wordpress-tests-lib';
-	}
-
-	// If the tests' includes directory does not exist, try a relative path to Core tests directory.
-	if ( ! file_exists( $tests_dir . '/includes/' ) ) {
-		$tests_dir = '../../../../../../../../tests/phpunit';
-	}
-
-	// Check it again. If it doesn't exist, stop here and post a message as to why we stopped.
-	if ( ! file_exists( $tests_dir . '/includes/' ) ) {
-		trigger_error( 'Unable to run the integration tests, because the WordPress test suite could not be located.', E_USER_ERROR ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error -- Valid use case for our testing suite.
-	}
-
-	// Strip off the trailing directory separator, if it exists.
-	return rtrim( $tests_dir, DIRECTORY_SEPARATOR );
-}
+init_test_suite();
 
 /**
  * Bootstraps the integration testing environment with WordPress.
- *
- * @param string $wp_tests_dir The directory path to the WordPress testing environment.
  */
-function bootstrap_integration_suite( $wp_tests_dir ) {
+function bootstrap_integration_suite() {
 	// Set MULTISITE constant when running the Multisite group of tests.
 	if ( BootstrapManager::isGroup( 'Multisite' ) && ! defined( 'MULTISITE' ) ) {
 		define( 'MULTISITE', true );
 	}
+
+	$wp_tests_dir = WPIntegration\get_path_to_wp_test_dir();
 
 	// Give access to tests_add_filter() function.
 	require_once $wp_tests_dir . '/includes/functions.php';
@@ -76,7 +51,7 @@ function bootstrap_integration_suite( $wp_tests_dir ) {
 	}
 
 	// Start up the WP testing environment.
-	require_once $wp_tests_dir . '/includes/bootstrap.php';
+	WPIntegration\bootstrap_it();
 }
 
-bootstrap_integration_suite( get_wp_tests_dir() );
+bootstrap_integration_suite();
