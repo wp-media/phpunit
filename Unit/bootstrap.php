c6cea949b3a0174be6f19728037b2bbd44f29c11
Rémy Perona <remperona@gmail.com>
2021-03-18 08:41:02 -0400

    require wp-test-utils bootstrap

--- Unit/bootstrap.php
+++ Unit/bootstrap.php
@@ -4,27 +4,9 @@ namespace WPMedia\PHPUnit\Unit;
 
 use function WPMedia\PHPUnit\init_test_suite;
 
+require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/BrainMonkey/bootstrap.php';
 require_once dirname( dirname( __FILE__ ) ) . '/bootstrap-functions.php';
-init_test_suite( 'Unit' );
-
-if ( ! defined( 'MINUTE_IN_SECONDS' ) ) {
-	define( 'MINUTE_IN_SECONDS', 60 );
-}
-if ( ! defined( 'HOUR_IN_SECONDS' ) ) {
-	define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
-}
-if ( ! defined( 'DAY_IN_SECONDS' ) ) {
-	define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
-}
-if ( ! defined( 'WEEK_IN_SECONDS' ) ) {
-	define( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS );
-}
-if ( ! defined( 'MONTH_IN_SECONDS' ) ) {
-	define( 'MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS );
-}
-if ( ! defined( 'YEAR_IN_SECONDS' ) ) {
-	define( 'YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS );
-}
+init_test_suite();
 
 // Bootstrap the wp-media/phpunit-{add-on}.
 if (
