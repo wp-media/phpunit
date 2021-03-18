0818677a6e9015729ef79e7d6a5d0f2764a5d6de
Rémy Perona <remperona@gmail.com>
2021-03-18 08:40:41 -0400

    update default directories names

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -14,8 +14,8 @@ SKIP_DB_CREATE=${6-false}
 
 TMPDIR=${TMPDIR-/tmp}
 TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
-WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
+WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/tests/phpunit}
+WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress-develop}
 
 download() {
     if [ `which curl` ]; then
