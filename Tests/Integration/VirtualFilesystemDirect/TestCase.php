fe2fc2d5eceb2409826301d8516a7cb715867345
Rémy Perona <remperona@gmail.com>
2022-04-08 16:22:47 -0400

    fix tests

--- Tests/Integration/VirtualFilesystemDirect/TestCase.php
+++ Tests/Integration/VirtualFilesystemDirect/TestCase.php
@@ -7,6 +7,12 @@ use WPMedia\PHPUnit\Integration\VirtualFilesystemTestCase;
 abstract class TestCase extends VirtualFilesystemTestCase {
 	protected $path_to_test_data = 'structure.php';
 
+	public function set_up() {
+		parent::set_up();
+
+		$this->init();
+	}
+
 	public function getPathToFixturesDir() {
 		return WPMEDIA_PHPUNIT_ROOT_DIR . '/Tests/Fixtures/';
 	}
--- Tests/Unit/VirtualFilesystemDirect/TestCase.php
+++ Tests/Unit/VirtualFilesystemDirect/TestCase.php
@@ -7,6 +7,12 @@ use WPMedia\PHPUnit\Unit\VirtualFilesystemTestCase;
 abstract class TestCase extends VirtualFilesystemTestCase {
 	protected $path_to_test_data = 'structure.php';
 
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->init();
+	}
+
 	public function getPathToFixturesDir() {
 		return WPMEDIA_PHPUNIT_ROOT_DIR . '/Tests/Fixtures/';
 	}
