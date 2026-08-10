e27454feb173f848a4a01f29a5495935aa731bba
fetachino <abald001@icloud.com>
2026-08-10 17:05:32 -0400

    Fix PHP minimum version readiness check

--- Tests/Unit/testBootstrap.php
+++ Tests/Unit/testBootstrap.php
@@ -3,6 +3,7 @@
 namespace WPMedia\PHPUnit\Tests\Unit;
 
 use WPMedia\PHPUnit\Unit\TestCase;
+use function WPMedia\PHPUnit\check_readiness;
 
 class Test_Bootstrap extends TestCase {
 
@@ -15,5 +16,25 @@ class Test_Bootstrap extends TestCase {
 	function testShouldReturnTrueWhenRootBootstrapFileIsInMemory() {
 		$this->assertTrue( function_exists( __NAMESPACE__ . '\is_unit_test_bootstrap' ) );
 		$this->assertTrue( is_unit_test_bootstrap() );
+	}
+
+	function testShouldAcceptMinimumSupportedPhpVersion() {
+		$this->assertNull( check_readiness( '7.4.0' ) );
+	}
+
+	function testShouldRejectPhpVersionBelowSupportedMinimum() {
+		$error = null;
+		set_error_handler(
+			function ( $errno, $errstr ) use ( &$error ) {
+				$error = compact( 'errno', 'errstr' );
+				return true;
+			}
+		);
+
+		check_readiness( '7.3.99' );
+		restore_error_handler();
+
+		$this->assertSame( E_USER_ERROR, $error['errno'] );
+		$this->assertSame( 'Test Suite requires PHP 7.4 or higher.', $error['errstr'] );
 	}
 }
